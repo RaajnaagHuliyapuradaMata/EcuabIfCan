71356553c05bc832fccbe2f20bcefe55137bbfd2
root <nagaraja.hm.0001@gmail.com>
2022-01-18 10:32:30 +0100

    Update: submodule CanIf

--- LATEST/CanIf.cpp
+++ LATEST/CanIf.cpp
@@ -5,3 +5,96 @@ class_CanIf CanIf;
 FUNC(void, CANIF_CODE) class_CanIf::InitFunction(void){
 }
 
+FUNC(void, CANIF_CODE) class_CanIf::GetControllerMode(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::SetControllerMode(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::Transmit(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CancelTransmit(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::ReadRxPduData(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::ReadTxNotifStatus(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::ReadRxNotifStatus(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::GetPduMode(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::SetPduMode(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::GetVersionInfo(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::SetDynamicTxId(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::GetTrcvMode(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::SetTrcvMode(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::GetTrcvWakeupReason(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::SetTrcvWakeupMode(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CheckWakeup(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CheckValidation(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::GetTxConfirmationState(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::ClearTrcvWufFlag(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CheckTrcvWakeFlag(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CheckBaudRate(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::ChangeBaudRate(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbTxConfirmation(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbRxIndication(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbCancelTxConfirmation(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbControllerBusOff(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbConfirmPnAvailability(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbClearTrcvWufFlagIndication(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbCheckTrcvWakeFlagIndication(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbControllerModeIndication(void){
+}
+
+FUNC(void, CANIF_CODE) class_CanIf::CbTrcvModeIndication(void){
+}
+
--- LATEST/CanIf.h
+++ LATEST/CanIf.h
@@ -5,7 +5,38 @@
 
 class class_CanIf{
    public:
-      FUNC(void, CANIF_CODE) InitFunction(void);
+      FUNC(void, CANIF_CODE) InitFunction                  (void);
+      FUNC(void, CANIF_CODE) GetControllerMode             (void);
+      FUNC(void, CANIF_CODE) SetControllerMode             (void);
+      FUNC(void, CANIF_CODE) Transmit                      (void);
+      FUNC(void, CANIF_CODE) CancelTransmit                (void);
+      FUNC(void, CANIF_CODE) ReadRxPduData                 (void);
+      FUNC(void, CANIF_CODE) ReadTxNotifStatus             (void);
+      FUNC(void, CANIF_CODE) ReadRxNotifStatus             (void);
+      FUNC(void, CANIF_CODE) GetPduMode                    (void);
+      FUNC(void, CANIF_CODE) SetPduMode                    (void);
+      FUNC(void, CANIF_CODE) GetVersionInfo                (void);
+      FUNC(void, CANIF_CODE) SetDynamicTxId                (void);
+      FUNC(void, CANIF_CODE) GetTrcvMode                   (void);
+      FUNC(void, CANIF_CODE) SetTrcvMode                   (void);
+      FUNC(void, CANIF_CODE) GetTrcvWakeupReason           (void);
+      FUNC(void, CANIF_CODE) SetTrcvWakeupMode             (void);
+      FUNC(void, CANIF_CODE) CheckWakeup                   (void);
+      FUNC(void, CANIF_CODE) CheckValidation               (void);
+      FUNC(void, CANIF_CODE) GetTxConfirmationState        (void);
+      FUNC(void, CANIF_CODE) ClearTrcvWufFlag              (void);
+      FUNC(void, CANIF_CODE) CheckTrcvWakeFlag             (void);
+      FUNC(void, CANIF_CODE) CheckBaudRate                 (void);
+      FUNC(void, CANIF_CODE) ChangeBaudRate                (void);
+      FUNC(void, CANIF_CODE) CbTxConfirmation              (void);
+      FUNC(void, CANIF_CODE) CbRxIndication                (void);
+      FUNC(void, CANIF_CODE) CbCancelTxConfirmation        (void);
+      FUNC(void, CANIF_CODE) CbControllerBusOff            (void);
+      FUNC(void, CANIF_CODE) CbConfirmPnAvailability       (void);
+      FUNC(void, CANIF_CODE) CbClearTrcvWufFlagIndication  (void);
+      FUNC(void, CANIF_CODE) CbCheckTrcvWakeFlagIndication (void);
+      FUNC(void, CANIF_CODE) CbControllerModeIndication    (void);
+      FUNC(void, CANIF_CODE) CbTrcvModeIndication          (void);
 };
 
 extern class_CanIf CanIf;
