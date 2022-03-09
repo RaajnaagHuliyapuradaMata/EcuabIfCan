95a5e2c8c7f7b712aaf56f2b0a06040753f74591
root <nagaraja.hm.0001@gmail.com>
2022-03-09 12:53:21 +0100

    Update: submodule CanIf

--- LATEST/CanIf_EcuM.h
+++ LATEST/CanIf_EcuM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infEcuMClient *gptrinfEcuMClient_CanIf;
+extern infEcuMClient* gptrinfEcuMClient_CanIf;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
--- LATEST/CanIf_SchM.h
+++ LATEST/CanIf_SchM.h
@@ -32,7 +32,7 @@
 /*****************************************************/
 /* OBJECTS                                           */
 /*****************************************************/
-extern infSchMClient *gptrinfSchMClient_CanIf;
+extern infSchMClient* gptrinfSchMClient_CanIf;
 
 /*****************************************************/
 /* FUNCTIONS                                         */
