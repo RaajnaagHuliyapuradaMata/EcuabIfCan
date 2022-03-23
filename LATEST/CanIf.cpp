415effb754a698d5566be55101a4f1f7c3ea5141
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:06:43 +0100

    Update: AUTOSAR basic software ECU abstraction module CanIf

--- LATEST/CanIf.cpp
+++ LATEST/CanIf.cpp
@@ -40,6 +40,16 @@ class module_CanIf:
       FUNC(void, CANIF_CODE) DeInitFunction (void);
       FUNC(void, CANIF_CODE) GetVersionInfo (void);
       FUNC(void, CANIF_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, CANIF_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
