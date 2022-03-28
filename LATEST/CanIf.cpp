1e7a2ac3804bd2143fdf203f6cb1529f755bb2de
root <nagaraja.hm.0001@gmail.com>
2022-03-28 12:46:25 +0200

    Update: AUTOSAR basic software ECU abstraction module CanIf

--- LATEST/CanIf.cpp
+++ LATEST/CanIf.cpp
@@ -79,18 +79,56 @@ FUNC(void, CANIF_CODE) module_CanIf::DeInitFunction(void){
    CanIf.IsInitDone = E_NOT_OK;
 }
 
-FUNC(void, CANIF_CODE) module_CanIf::GetVersionInfo(void){
+FUNC(void, CANIF_CODE) module_CanIf::GetVersionInfo(
+   CONSTP2VAR(Std_TypeVersionInfo, CANIF_VAR, CANIF_CONST) lptrVersionInfo
+){
+   if(NULL_PTR == lptrVersionInfo){
 #if(STD_ON == CanIf_DevErrorDetect)
-//TBD: API parameter check
-   Det_ReportError(
-   );
+      Det_ReportError(
+      );
 #endif
+   }
+   else{
+   }
 }
 
 FUNC(void, CANIF_CODE) module_CanIf::MainFunction(void){
 }
 
-#include "CanIf_Unused.hpp"
+class class_CanIf_Unused{
+   public:
+      FUNC(void, CANIF_CODE) GetControllerMode             (void);
+      FUNC(void, CANIF_CODE) SetControllerMode             (void);
+      FUNC(void, CANIF_CODE) Transmit                      (void);
+      FUNC(void, CANIF_CODE) CancelTransmit                (void);
+      FUNC(void, CANIF_CODE) ReadRxPduData                 (void);
+      FUNC(void, CANIF_CODE) ReadTxNotifStatus             (void);
+      FUNC(void, CANIF_CODE) ReadRxNotifStatus             (void);
+      FUNC(void, CANIF_CODE) GetPduMode                    (void);
+      FUNC(void, CANIF_CODE) SetPduMode                    (void);
+      FUNC(void, CANIF_CODE) GetVersionInfo                (void);
+      FUNC(void, CANIF_CODE) SetDynamicTxId                (void);
+      FUNC(void, CANIF_CODE) GetTrcvMode                   (void);
+      FUNC(void, CANIF_CODE) SetTrcvMode                   (void);
+      FUNC(void, CANIF_CODE) GetTrcvWakeupReason           (void);
+      FUNC(void, CANIF_CODE) SetTrcvWakeupMode             (void);
+      FUNC(void, CANIF_CODE) CheckWakeup                   (void);
+      FUNC(void, CANIF_CODE) CheckValidation               (void);
+      FUNC(void, CANIF_CODE) GetTxConfirmationState        (void);
+      FUNC(void, CANIF_CODE) ClearTrcvWufFlag              (void);
+      FUNC(void, CANIF_CODE) CheckTrcvWakeFlag             (void);
+      FUNC(void, CANIF_CODE) CheckBaudRate                 (void);
+      FUNC(void, CANIF_CODE) ChangeBaudRate                (void);
+      FUNC(void, CANIF_CODE) CbTxConfirmation              (void);
+      FUNC(void, CANIF_CODE) CbRxIndication                (void);
+      FUNC(void, CANIF_CODE) CbCancelTxConfirmation        (void);
+      FUNC(void, CANIF_CODE) CbControllerBusOff            (void);
+      FUNC(void, CANIF_CODE) CbConfirmPnAvailability       (void);
+      FUNC(void, CANIF_CODE) CbClearTrcvWufFlagIndication  (void);
+      FUNC(void, CANIF_CODE) CbCheckTrcvWakeFlagIndication (void);
+      FUNC(void, CANIF_CODE) CbControllerModeIndication    (void);
+      FUNC(void, CANIF_CODE) CbTrcvModeIndication          (void);
+};
 
 FUNC(void, CANIF_CODE) class_CanIf_Unused::GetControllerMode(void){
 }
