e129dbc93557a26104ab2f5e23ddfa8681842077
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-27 10:43:23 +0000

    Update: AUTOSAR basic software ECU abstraction module CanIf

---
+++ LATEST/CanIf_core.hpp
@@ -0,0 +1,76 @@
+#pragma once
+/******************************************************************************/
+/* File   : CanIf_core.hpp                                                    */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #INCLUDES                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+
+/******************************************************************************/
+/* MACROS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* TYPEDEFS                                                                   */
+/******************************************************************************/
+class class_CanIf_Functionality{
+   public:
+      FUNC(void, CANIF_CODE) GetControllerMode             (void);
+      FUNC(void, CANIF_CODE) SetControllerMode             (void);
+      FUNC(void, CANIF_CODE) Transmit                      (void);
+      FUNC(void, CANIF_CODE) CancelTransmit                (void);
+      FUNC(void, CANIF_CODE) ReadRxPduData                 (void);
+      FUNC(void, CANIF_CODE) ReadTxNotifStatus             (void);
+      FUNC(void, CANIF_CODE) ReadRxNotifStatus             (void);
+      FUNC(void, CANIF_CODE) GetPduMode                    (void);
+      FUNC(void, CANIF_CODE) SetPduMode                    (void);
+      FUNC(void, CANIF_CODE) GetVersionInfo                (void);
+      FUNC(void, CANIF_CODE) SetDynamicTxId                (void);
+      FUNC(void, CANIF_CODE) GetTrcvMode                   (void);
+      FUNC(void, CANIF_CODE) SetTrcvMode                   (void);
+      FUNC(void, CANIF_CODE) GetTrcvWakeupReason           (void);
+      FUNC(void, CANIF_CODE) SetTrcvWakeupMode             (void);
+      FUNC(void, CANIF_CODE) CheckWakeup                   (void);
+      FUNC(void, CANIF_CODE) CheckValidation               (void);
+      FUNC(void, CANIF_CODE) GetTxConfirmationState        (void);
+      FUNC(void, CANIF_CODE) ClearTrcvWufFlag              (void);
+      FUNC(void, CANIF_CODE) CheckTrcvWakeFlag             (void);
+      FUNC(void, CANIF_CODE) CheckBaudRate                 (void);
+      FUNC(void, CANIF_CODE) ChangeBaudRate                (void);
+      FUNC(void, CANIF_CODE) CbTxConfirmation              (void);
+      FUNC(void, CANIF_CODE) CbRxIndication                (void);
+      FUNC(void, CANIF_CODE) CbCancelTxConfirmation        (void);
+      FUNC(void, CANIF_CODE) CbControllerBusOff            (void);
+      FUNC(void, CANIF_CODE) CbConfirmPnAvailability       (void);
+      FUNC(void, CANIF_CODE) CbClearTrcvWufFlagIndication  (void);
+      FUNC(void, CANIF_CODE) CbCheckTrcvWakeFlagIndication (void);
+      FUNC(void, CANIF_CODE) CbControllerModeIndication    (void);
+      FUNC(void, CANIF_CODE) CbTrcvModeIndication          (void);
+};
+
+/******************************************************************************/
+/* CONSTS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* PARAMS                                                                     */
+/******************************************************************************/
+
+/******************************************************************************/
+/* OBJECTS                                                                    */
+/******************************************************************************/
+
+/******************************************************************************/
+/* FUNCTIONS                                                                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
