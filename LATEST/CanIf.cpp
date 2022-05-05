9352e25f38594f3ef09c3774e30df6acb59aba9c
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:07:07 +0000

    Update: AUTOSAR basic software ECU abstraction module CanIf

--- LATEST/CanIf.cpp
+++ LATEST/CanIf.cpp
@@ -85,12 +85,10 @@ FUNC(void, CANIF_CODE) module_CanIf::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgCanIf;
          }
       }
