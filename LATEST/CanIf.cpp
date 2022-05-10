d239d00b2347da28832d45284030523f538a24d7
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-10 10:08:09 +0000

    Update: AUTOSAR basic software ECU abstraction module CanIf

--- LATEST/CanIf.cpp
+++ LATEST/CanIf.cpp
@@ -89,7 +89,10 @@ FUNC(void, CANIF_CODE) module_CanIf::InitFunction(
    CONSTP2CONST(CfgModule_TypeAbstract, CANIF_CONFIG_DATA, CANIF_APPL_CONST) lptrCfgModule
 ){
 #if(STD_ON == CanIf_InitCheck)
-   if(E_OK != IsInitDone){
+   if(
+         E_OK
+      != IsInitDone
+   ){
 #endif
       if(NULL_PTR != lptrCfgModule){
          if(STD_HIGH){
@@ -127,7 +130,10 @@ FUNC(void, CANIF_CODE) module_CanIf::InitFunction(
 
 FUNC(void, CANIF_CODE) module_CanIf::DeInitFunction(void){
 #if(STD_ON == CanIf_InitCheck)
-   if(E_OK == IsInitDone){
+   if(
+         E_OK
+      == IsInitDone
+   ){
 #endif
 #if(STD_ON == CanIf_InitCheck)
       IsInitDone = E_NOT_OK;
@@ -147,7 +153,10 @@ FUNC(void, CANIF_CODE) module_CanIf::DeInitFunction(void){
 
 FUNC(void, CANIF_CODE) module_CanIf::MainFunction(void){
 #if(STD_ON == CanIf_InitCheck)
-   if(E_OK == IsInitDone){
+   if(
+         E_OK
+      == IsInitDone
+   ){
 #endif
 #if(STD_ON == CanIf_InitCheck)
    }
