e14159f36a2c189dbb1c1392c3ae812303c39163
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-08 21:18:32 +0000

    Update: AUTOSAR basic software ECU abstraction module CanIf

--- LATEST/CanIf.cpp
+++ LATEST/CanIf.cpp
@@ -75,7 +75,7 @@ CONSTP2VAR(infSchMClient, CANIF_VAR, CANIF_CONST) gptrinfSchMClient_CanIf = &Can
 VAR(module_CanIf, CANIF_VAR) CanIf(
    {
 #if(STD_ON == _ReSIM)
-// char strModuleName[6];
+   char strModuleName[6] = "CANIF";
 #else
 #endif
          CANIF_AR_RELEASE_VERSION_MAJOR
