19dd993bc2a48afe6b2222827b3bb6d283118d61
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-06 17:29:21 +0000

    Update: AUTOSAR basic software ECU abstraction module for CAN communication protocol stack - interface layer

--- LATEST/CanIf.cpp
+++ LATEST/CanIf.cpp
@@ -7,9 +7,7 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "Module.hpp"
-#include "CfgCanIf.hpp"
-#include "CanIf_core.hpp"
-#include "infCanIf_Exp.hpp"
+#include "CanIf.hpp"
 #include "infCanIf_Imp.hpp"
 
 /******************************************************************************/
@@ -32,27 +30,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class module_CanIf:
-      INTERFACES_EXPORTED_CANIF
-   ,  public abstract_module
-   ,  public class_CanIf_Functionality
-{
-   private:
-/******************************************************************************/
-/* OBJECTS                                                                    */
-/******************************************************************************/
-            infPduRClient_Lo        infPduRClient_CanIf;
-
-   public:
-      FUNC(void, CANIF_CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, CANIF_CONFIG_DATA, CANIF_APPL_CONST) lptrCfgModule
-      );
-      FUNC(void, CANIF_CODE) DeInitFunction (void);
-      FUNC(void, CANIF_CODE) MainFunction   (void);
-      CANIF_CORE_FUNCTIONALITIES
-};
-
-extern VAR(module_CanIf, CANIF_VAR) CanIf;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
